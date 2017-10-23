b866dcb1d23dba4bb543378cf18b71617ec03ec5
Matthew Jones <mcolinj@mac.com>
2017-10-23 13:11:04 -0400

    Add helper function and some self testing.

--- Week1.py
+++ Week1.py
@@ -1,6 +1,28 @@
 # French verb conjugator
 import sys
 
+def ends_in_er(word):
+        """
+        Return True if the string word ends in 'er', otherwise return False.
+        This does not work if there is whitespace at the end of the string.
+        Maybe we could consider taking care of that in this function just to
+        be robust.
+        """
+        return (word[-2:] == 'er')
+
+def test_functions():
+        if not ends_in_er('beer'):
+                print("FAIL")
+        if ends_in_er('carrot'):
+                print("FAIL")
+        #  Following test could catch something that we change
+        if ends_in_er('amanecer\n'):
+                print("FAIL.  Currently not stripping whitespace")
+
+
+# run the self-test for our functions
+test_functions()
+
 # prompt user
 print ("Enter verb in infinitive form")
 
@@ -20,3 +42,5 @@ for verb in sys.stdin:
 		print ("ils/elles " + beg + "ent")
 	# prompt user
 	print ("Enter verb in infinitive form")
+
+
